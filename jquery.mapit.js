1311c034cc0fd80ffbcb6a9cbdd7f6921cd6f415
supersabillon <jsabillon@beachbody.com>
2015-07-06 13:59:19 -0700

    wrap element in a tag

--- jquery.mapit.js
+++ jquery.mapit.js
@@ -3,15 +3,20 @@
 
 	$.fn.mapit = function() {
 
+		var $mapIt = this;
 
 	  	return this.each(function() {
 	  	  	createOverlays();
+	  	  	createWrappers();
 	  	});
 
 
+	  	function createWrappers() {
+	  		$mapIt.wrap("<a href='#'></a>")
+	  	}
+
 	  	function createOverlays(){
-	  		var doc = document,
-	  			overlay = doc.createElement("div");
+	  		var overlay = document.createElement("div");
 
 	  			$(overlay).addClass('overlay').css("display", "none");
 
